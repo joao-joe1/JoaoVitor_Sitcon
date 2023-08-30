2c1fc9ffe9ddad98fea261ac1c874173744baaf6
Joe <joaokingteam@hotmail.com>
2023-08-29 21:09:43 -0300

    feats

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,17 +14,10 @@ const initialPagination = 1;
 export default async function Home() {
   const res = await fetch("http://localhost:3001/get-patiens")
   const json = await res.json();
-  // console.log(json)
-
-  const page = 1
-  const amountOfPatientsPerPage = 10
-
-  const firstIdPatientOfPage = (page - 1) * amountOfPatientsPerPage + 1
-  const lastIdPatientOfPage = (page - 1) * amountOfPatientsPerPage + 10
 
   return (
     <>
-      <h1>Home</h1>
+      {/* <h1>Home</h1> */}
       <PatientTable />
     </>
   )
--- frontend/src/layouts/Table/index.tsx
+++ frontend/src/layouts/Table/index.tsx
@@ -1,18 +1,17 @@
-'use client';
-
+'use client'
 import React, { useEffect, useState } from 'react';
-
-import styles from './styles.module.css'
-
+import styles from './styles.module.css';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { RiSearchLine } from 'react-icons/ri';
 
 interface PatientTableProps { }
 
 type TPatient = {
-  id: number,
-  nome: string,
-  dataNasc: string,
-  cpf: string
-}
+  id: number;
+  nome: string;
+  dataNasc: string;
+  cpf: string;
+};
 
 const formatDate = (dateString: string): string => {
   const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
@@ -22,76 +21,88 @@ const formatDate = (dateString: string): string => {
 const INITIAL_PAGINATION_VALUE = 1;
 
 export default function PatientTable({ }: PatientTableProps) {
-  const [data, setData] = useState<TPatient[]>([])
+  const [data, setData] = useState<TPatient[]>([]);
   const [loading, setIsLoading] = useState(true);
   const [page, setPage] = useState(INITIAL_PAGINATION_VALUE);
 
   useEffect(() => {
-    console.log("useEffect has hired ")
     async function getPatientsPerPage() {
       try {
-        const data = await fetch(`http://localhost:3001/get-patiens?page=${page}`)
-        console.log("response inside getPatientsPerPage has value: ", data)
-        const json = await data.json()
-        console.log("json value:", json)
+        const data = await fetch(`http://localhost:3001/get-patiens?page=${page}`);
+        const json = await data.json();
 
-        setIsLoading(false)
-        setData(json.data)
+        setIsLoading(false);
+        setData(json.data);
       } catch (error) {
-        console.error(error)
-        throw error
+        console.error(error);
+        throw error;
       }
     }
 
-    getPatientsPerPage()
+    getPatientsPerPage();
 
-    return () => {
+    return () => { };
+  }, [page]);
 
-    }
-  }, [page])
-
-  if (loading) return <h1>Loading....</h1>
-
-  console.log("data value: ", data)
+  if (loading) return <h1>Loading....</h1>;
 
   return (
     <>
-      <table>
+      <div className={styles.searchBar}>
+        <div className={styles.searchIcon}>
+          <RiSearchLine />
+        </div>
+        <input type="text" placeholder="Pesquisar" />
+      </div>
+
+      <table className={styles.patientTable}>
         <thead>
           <tr>
-            <th>Nome do Paciente</th>
-            <th>Data de Nascimento</th>
+            <th>Paciente</th>
+            <th>Nascimento</th>
             <th>CPF</th>
-            <th>Ação</th>
+            <th>Ações</th>
           </tr>
         </thead>
         <tbody>
           {data.map((patient, index) => (
-            <tr key={index}>
+            <tr key={index} className={styles.patientRow}>
               <td>{patient.nome}</td>
               <td>{formatDate(patient.dataNasc)}</td>
               <td>{patient.cpf}</td>
               <td>
-                <button>Ação</button>
+                <button className={styles.actionButton}><b>Prosseguir</b></button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
 
+      <div className={styles.pagination} aria-label="pagination">
+        {page > 1 && (
+          <button onClick={() => setPage(page => page - 1)}>
+            <FiChevronLeft /> {/* Setinha para a esquerda */}
+          </button>
+        )}
 
-      {!(data.length === 0) ? (
-        data.map((patient, i) => <pre key={`data_${i}`}>{JSON.stringify(patient)}</pre>)
-      ) : (<h1>Não tem usuários para mostragem</h1>)}
-
-      <div aria-label='pagination'>
-        <button onClick={() => setPage(page => page - 1)}>Previous</button>
-        <div >
-          {/* {Array.from({ length: 10 }, (_, i) => <span key={`pag_${i}`}>{i + 1}</span>)} */}
+        <div className={styles.pageInputs}>
+          {Array.from({ length: 10 }, (_, i) => (
+            <button
+              key={`pag_${i}`}
+              className={styles.pageButton}
+              onClick={() => setPage(i + 1)}
+            >
+              <b>{i + 1}</b>
+            </button>
+          ))}
         </div>
-        <button onClick={() => setPage(page => page + 1)}>Next</button>
-      </div >
-    </>
 
+        {page < data.length && (
+          <button onClick={() => setPage(page => page + 1)}>
+            <FiChevronRight /> {/* Setinha para a direita */}
+          </button>
+        )}
+      </div>
+    </>
   );
-};
+}
